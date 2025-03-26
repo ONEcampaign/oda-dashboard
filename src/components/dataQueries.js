b1878bdf25015a2d6500fa59e6543aa7b30857c8
mharoruiz <88669820+mharoruiz@users.noreply.github.com>
2025-03-26 22:16:03 +0100

    Update data queries

--- src/components/dataQueries.js
+++ src/components/dataQueries.js
@@ -1,20 +1,377 @@
 import {FileAttachment} from "observablehq:stdlib";
 import {DuckDBClient} from "npm:@observablehq/duckdb";
-import {name2CodeMap, getKeysByValue} from "./utils.js";
+import {name2CodeMap, getNameByCode, generateIndicatorMap} from "./utils.js";
 
 
 const db = await DuckDBClient.of({
+    financing: FileAttachment("../data/scripts/financing.parquet"),
+    recipients: FileAttachment("../data/scripts/recipients.parquet"),
     gender: FileAttachment("../data/scripts/gender.parquet"),
+    gni_table: FileAttachment("../data/scripts/gni_table.parquet"),
     current_conversion_table: FileAttachment("../data/scripts/current_conversion_table.csv"),
     constant_conversion_table: FileAttachment("../data/scripts/constant_conversion_table.csv")
 });
 
-const donorOptions = await FileAttachment("../data/settings/donors.json").json()
-const recipientOptions = await FileAttachment("../data/settings/recipients.json").json()
+const donorOptions = await FileAttachment("../data/analysis_tools/donor_mapping.json").json()
+const recipientOptions = await FileAttachment("../data/analysis_tools/recipient_mapping.json").json()
+const indicatorOptions = await FileAttachment("../data/analysis_tools/indicators.json").json()
 
 const donorMapping = name2CodeMap(donorOptions)
 const recipientMapping = name2CodeMap(recipientOptions)
 
+
+
+//  FINANCING VIEW
+export function financingQueries(
+    donor,
+    indicator,
+    currency,
+    prices,
+    timeRange
+) {
+
+    const indicatorMapping = generateIndicatorMap(indicatorOptions, "financing")
+
+    const absolute = absoluteFinancingQuery(
+        donor,
+        indicator,
+        indicatorMapping,
+        currency,
+        prices,
+        timeRange
+    );
+
+    const relative = relativeFinancingQuery(
+        donor,
+        indicator,
+        indicatorMapping,
+        currency,
+        prices,
+        timeRange
+    );
+
+    return {absolute, relative};
+
+}
+
+async function absoluteFinancingQuery(
+    donor,
+    indicator,
+    indicatorMapping,
+    currency,
+    prices,
+    timeRange
+) {
+
+    const query = await db.query(
+        `
+            WITH filtered AS (
+                SELECT 
+                    year, 
+                    donor_code AS donor,
+                    indicator, 
+                    (value * 1.1 / 1.1) AS value
+                FROM financing
+                WHERE 
+                    donor_code IN (${donor})
+                    AND indicator = ${indicator}
+                    AND year between ${timeRange[0]} AND ${timeRange[1]}
+            ),
+            conversion AS (
+                SELECT
+                    year,
+                    ${prices === "constant" ? "dac_code AS donor," : ""}
+                    ${currency}_${prices} AS factor
+                FROM
+                    ${prices}_conversion_table
+                    ${prices === "constant" ? `WHERE dac_code IN (${donor})` : ""}
+            ),
+            joined AS (
+                SELECT
+                    f.year,
+                    f.indicator,
+                    f.value * c.factor AS converted_value
+                FROM filtered f
+                    JOIN conversion c
+                ON f.year = c.year
+                    ${prices === "constant" ? "AND f.donor = c.donor" : ""}
+            )
+            SELECT
+                year AS Year,
+                '${getNameByCode(donorMapping, donor)}' AS donor,
+                '${getNameByCode(indicatorMapping, indicator)}' AS indicator,
+                CASE
+                    WHEN year < 2018 THEN 'Flow'
+                    WHEN year >= 2018 THEN 'Grant equivalent'
+                END AS Type,
+                SUM(converted_value) as Value,
+                '${currency} ${prices} million' as Unit,
+                'OECD DAC1' AS Source
+            FROM joined
+            GROUP BY year, indicator
+            ORDER BY year
+        `
+    )
+
+    return query.toArray().map((row) => ({
+        ...row
+    }));
+
+}
+
+
+async function relativeFinancingQuery(
+    donor,
+    indicator,
+    indicatorMapping,
+    currency,
+    prices,
+    timeRange
+) {
+
+    const query = await db.query(
+        `
+            WITH filtered AS (
+                SELECT 
+                    year,
+                    SUM(value * 1.1 / 1.1) AS value
+                FROM financing
+                WHERE 
+                    donor_code IN (${donor})
+                    AND indicator = (${indicator})
+                    AND year BETWEEN ${timeRange[0]} AND ${timeRange[1]}
+                GROUP BY year
+            ),
+            gni AS (
+                SELECT 
+                    year,
+                    SUM(value * 1.1 / 1.1) AS gni
+                FROM gni_table
+                WHERE
+                    donor_code IN (${donor})
+                    AND year BETWEEN ${timeRange[0]} AND ${timeRange[1]}
+                GROUP BY year
+            ), 
+            total AS (
+                SELECT
+                    year,
+                    SUM(value * 1.1 / 1.1) AS total_value
+                FROM financing
+                WHERE
+                    donor_code IN (${donor})
+                    AND indicator = ${indicatorMapping.get("Total ODA")}
+                    AND year BETWEEN ${timeRange[0]} AND ${timeRange[1]}
+                GROUP BY year
+                    
+                )
+            SELECT
+                f.year AS Year,
+                '${getNameByCode(donorMapping, donor)}' AS donor,
+                ${
+                    indicator === indicatorMapping.get("Total ODA")
+                        ? "f.value / g.gni * 100 AS Value,"
+                        : "f.value / t.total_value * 100 AS Value,"
+                }
+                CASE 
+                    WHEN f.year < 2018 THEN 'Flow'
+                    WHEN f.year >= 2018 THEN 'Grant equivalent'
+                END AS Type,
+                ${
+                    indicator === indicatorMapping.get("Total ODA")
+                        ? "'% of GNI' AS Unit,"
+                        : "'% of total aid' AS Unit,"
+                }
+                'OECD DAC1' AS Source
+            FROM filtered f
+            ${
+                indicator === indicatorMapping.get("Total ODA")
+                    ? "JOIN gni g ON f.year = g.year"
+                    : "JOIN total t ON f.year = t.year"
+            }
+            ORDER BY f.year
+        `
+    );
+
+    return query.toArray().map((row) => ({
+        ...row
+    }));
+
+}
+
+
+
+//  RECIPIENTS VIEW
+export function recipientsQueries(
+    donor,
+    recipient,
+    indicator,
+    currency,
+    prices,
+    timeRange
+) {
+
+    const indicatorMapping = generateIndicatorMap(indicatorOptions, "recipients")
+
+    const absolute = absoluteRecipientsQuery(
+        donor,
+        recipient,
+        indicator,
+        indicatorMapping,
+        currency,
+        prices,
+        timeRange
+    );
+
+    const relative = relativeRecipientsQuery(
+        donor,
+        recipient,
+        indicator,
+        indicatorMapping,
+        currency,
+        prices,
+        timeRange
+    );
+
+    return {absolute, relative};
+
+}
+
+async function absoluteRecipientsQuery(
+    donor,
+    recipient,
+    indicator,
+    indicatorMapping,
+    currency,
+    prices,
+    timeRange
+) {
+
+    const query = await db.query(
+        `
+            WITH filtered AS (
+                SELECT 
+                    year, 
+                    donor_code AS donor,
+                    indicator, 
+                    (value * 1.1 / 1.1) AS value
+                FROM recipients
+                WHERE 
+                    donor_code IN (${donor})
+                    AND recipient_code IN (${recipient})
+                    AND year between ${timeRange[0]} AND ${timeRange[1]} 
+                    ${
+                        indicator === indicatorMapping.get("Total ODA")
+                            ? ""
+                            : `AND indicator = ${indicator}`
+                    }
+            ),
+            conversion AS (
+                SELECT
+                    year,
+                    ${prices === "constant" ? "dac_code AS donor," : ""}
+                    ${currency}_${prices} AS factor
+                FROM
+                    ${prices}_conversion_table
+                    ${prices === "constant" ? `WHERE dac_code IN (${donor})` : ""}
+            ),
+            joined AS (
+                SELECT
+                    f.year,
+                    f.indicator,
+                    f.value * c.factor AS converted_value
+                FROM filtered f
+                    JOIN conversion c
+                ON f.year = c.year
+                    ${prices === "constant" ? "AND f.donor = c.donor" : ""}
+            )
+            SELECT
+                year AS Year,
+                '${getNameByCode(donorMapping, donor)}' AS donor,
+                '${getNameByCode(recipientMapping, recipient)}' AS recipient,
+                CASE
+                    WHEN indicator = 1 THEN 'Imputed multilateral'
+                    WHEN indicator = 0 THEN 'Bilateral'
+                END AS Indicator,
+                SUM(converted_value) as Value,
+                '${currency} ${prices} million' as Unit,
+                'OECD DAC2A' AS Source
+            FROM joined
+            GROUP BY year, indicator
+            ORDER BY year
+        `
+    )
+
+    return query.toArray().map((row) => ({
+        ...row
+    }));
+
+}
+
+
+async function relativeRecipientsQuery(
+    donor,
+    recipient,
+    indicator,
+    indicatorMapping,
+    currency,
+    prices,
+    timeRange
+) {
+
+    const query = await db.query(
+        `
+            WITH filtered AS (
+                SELECT 
+                    year,
+                    indicator,
+                    SUM(value * 1.1 / 1.1) AS value
+                FROM recipients
+                WHERE 
+                    donor_code IN (${donor})
+                    AND recipient_code IN (${recipient})
+                    AND year BETWEEN ${timeRange[0]} AND ${timeRange[1]}
+                    ${
+                        indicator === indicatorMapping.get("Total ODA")
+                                ? ""
+                                : `AND indicator = ${indicator}`
+                    }
+                GROUP BY year, indicator
+            ),
+            total AS (
+                SELECT 
+                    year,
+                    SUM(value * 1.1 / 1.1) AS total_value
+                FROM recipients
+                WHERE
+                    recipient_code IN (${recipient})
+                    AND year BETWEEN ${timeRange[0]} AND ${timeRange[1]}
+                GROUP BY year
+            )
+            SELECT
+                f.year AS Year,
+                '${getNameByCode(donorMapping, donor)}' AS donor,
+                '${getNameByCode(recipientMapping, recipient)}' AS recipient,
+                CASE
+                    WHEN indicator = 1 THEN 'Imputed multilateral'
+                    WHEN indicator = 0 THEN 'Bilateral'
+                END AS Indicator,
+                f.value / t.total_value * 100 AS Value,
+                '% of total aid' AS Unit,
+                'OECD DAC2A' AS Source
+            FROM filtered f
+            JOIN total t ON f.year = t.year
+            ORDER BY f.year
+        `
+    );
+
+    return query.toArray().map((row) => ({
+        ...row
+    }));
+
+}
+
+
+// GENDER VIEW
 export function genderQueries(
     donor,
     recipient,
@@ -61,7 +418,7 @@ async function absoluteGenderQuery(
                 SELECT 
                     year, 
                     donor_code AS donor,
-                    gender_code AS indicator, 
+                    indicator, 
                     (value * 1.1 / 1.1) AS value
                 FROM gender
                 WHERE 
@@ -71,8 +428,8 @@ async function absoluteGenderQuery(
                     AND 
                     ${
                         indicator === 3
-                            ? "gender_code IN (1, 2)"
-                            : `gender_code = ${indicator}`
+                            ? "indicator IN (1, 2)"
+                            : `indicator = ${indicator}`
                     }
             ),
             conversion AS (
@@ -94,14 +451,15 @@ async function absoluteGenderQuery(
                 ON f.year = c.year
                     ${prices === "constant" ? "AND f.donor = c.donor" : ""}
             )
-            SELECT 
+            SELECT
                 year AS Year,
-                '${getKeysByValue(donorMapping, donor)}' AS donor,
-                '${getKeysByValue(recipientMapping, recipient)}' AS recipient,
+                '${getNameByCode(donorMapping, donor)}' AS donor,
+                '${getNameByCode(recipientMapping, recipient)}' AS recipient,
                 CASE
                     WHEN indicator = 2 THEN 'Main focus'
                     WHEN indicator = 1 THEN 'Secondary focus'
                     WHEN indicator = 0 THEN 'Not targeted'
+                    WHEN indicator = 9 THEN 'Not screened'
                 END AS Indicator,
                 SUM(converted_value) as Value,
                 '${currency} ${prices} million' as Unit,
@@ -132,7 +490,7 @@ async function relativeGenderQuery(
             WITH filtered AS (
                 SELECT 
                     year,
-                    gender_code AS indicator,
+                    indicator,
                     SUM(value * 1.1 / 1.1) AS value
                 FROM gender
                 WHERE 
@@ -142,10 +500,10 @@ async function relativeGenderQuery(
                     AND
                     ${
                         indicator === 3
-                            ? "gender_code IN (1, 2)"
-                            : `gender_code = ${indicator}`
+                            ? "indicator IN (1, 2)"
+                            : `indicator = ${indicator}`
                     }
-                group by year, indicator
+                GROUP BY year, indicator
             ),
             total AS (
                 SELECT 
@@ -153,21 +511,22 @@ async function relativeGenderQuery(
                     SUM(value * 1.1 / 1.1) AS total_value
                 FROM gender
                 WHERE
-                    donor_code IN (${donor})
-                    AND recipient_code IN (${recipient})
+                    donor_code IN (${donorMapping.get("DAC members")}, ${donorMapping.get("non-DAC countries")})
+                    AND recipient_code IN (${recipientMapping.get("Developing countries")})
                     AND year BETWEEN ${timeRange[0]} AND ${timeRange[1]}
                 GROUP BY year
             )
             SELECT
                 f.year AS Year,
-                '${getKeysByValue(donorMapping, donor)}' AS donor,
-                '${getKeysByValue(recipientMapping, recipient)}' AS recipient,
+                '${getNameByCode(donorMapping, donor)}' AS donor,
+                '${getNameByCode(recipientMapping, recipient)}' AS recipient,
                 CASE
                     WHEN indicator = 2 THEN 'Main focus'
                     WHEN indicator = 1 THEN 'Secondary focus'
                     WHEN indicator = 0 THEN 'Not targeted'
+                    WHEN indicator = 9 THEN 'Not screened'
                 END AS Indicator,
-                ROUND(100.0 * f.value / t.total_value, 2) AS Value,
+                f.value / t.total_value * 100 AS Value,
                 '% of total aid' AS Unit,
                 'OECD Creditor Reporting System' AS Source
             FROM filtered f
