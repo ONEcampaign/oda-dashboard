f52d2b55e298163b71088d03123e2a0ba0168bc5
Jorge Rivera <jorge.rivera@one.org>
2025-04-03 12:09:11 +0200

    Add ONE Data header

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,21 +1,23 @@
+import { generateHeader } from "@one-data/observable-themes/header-template";
+
 // See https://observablehq.com/framework/config for documentation.
 export default {
   title: "ODA Dashboard",
 
-  head: '<link rel="icon" href="ONE-logo-favicon.png" type="image/png" sizes="32x32">' +
-      '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />',
+  head: `<link rel="icon" href="ONE-logo-favicon.png" type="image/png" sizes="32x32">'
+      <script src="npm:@one-data/observable-themes/header.js" defer></script>
+      <script src="npm:@one-data/observable-themes/footer.js" defer></script>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />`,
 
   root: "src",
   theme: ["light", "wide", "alt"],
   toc: false,
   sidebar: false,
   pager: false,
-  style: "style.css"
+  style: "style.css",
 
-  // Some additional configuration options and their defaults:
-  // theme: "default", // try "light", "dark", "slate", etc.
-  // header: "", // what to show in the header (HTML)
-  // footer: "Built with Observable.", // what to show in the footer (HTML)
+  header: generateHeader({title: "ODA Dashboard"}),
+   footer: "", // what to show in the footer (HTML)
   // sidebar: true, // whether to show the sidebar
   // pager: true, // whether to show previous & next links in the footer
   // output: "dist", // path to the output root for build
