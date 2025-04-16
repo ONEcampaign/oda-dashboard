a6dba61440749af7fe1d9423a12e17589a60ce95
Jorge Rivera <jorge.rivera@one.org>
2025-04-16 13:18:58 +0200

    Update covid.py
    format black

--- src/data/topic_page/covid.py
+++ src/data/topic_page/covid.py
@@ -5,16 +5,22 @@ from src.data.config import PATHS
 
 
 def covid_bi_multi_dac() -> pd.DataFrame:
-    df = pd.read_csv(PATHS.TOPIC_PAGE / "bi_plus_multi_health_spending_multiple_donors.csv")
+    df = pd.read_csv(
+        PATHS.TOPIC_PAGE / "bi_plus_multi_health_spending_multiple_donors.csv"
+    )
 
     codes = {v: k for k, v in provider_groupings()["dac_members"].items()}
 
     df["donor_code"] = df["donor_name"].map(codes)
 
     total = (
-        df.groupby(["year"], as_index=False)["covid_oda"].sum().assign(donor_name="DAC countries",
-                                                                       donor_code=20001))
+        df.groupby(["year"], as_index=False)["covid_oda"]
+        .sum()
+        .assign(donor_name="DAC countries", donor_code=20001)
+    )
 
-    df = pd.concat([df, total], ignore_index=True).rename(columns={"covid_oda": "value"})
+    df = pd.concat([df, total], ignore_index=True).rename(
+        columns={"covid_oda": "value"}
+    )
 
     return df
