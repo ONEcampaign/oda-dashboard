16d3e0ee5031fc1bd1205aeba5aeeec1d1317ded
mharoruiz <88669820+mharoruiz@users.noreply.github.com>
2025-04-25 17:17:17 +0200

    Update observablehq.config.js

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,28 +1,19 @@
-import { generateHeader } from "@one-data/observable-themes/header-template";
+import {generateHeader} from "@one-data/observable-themes/header";
+import {generateFooter} from "@one-data/observable-themes/footer";
+import {icon} from "@one-data/observable-themes/use-images";
 
-// See https://observablehq.com/framework/config for documentation.
 export default {
   title: "ODA Dashboard",
-
-  head: `<link rel="icon" href="ONE-logo-favicon.png" type="image/png" sizes="32x32">'
-      <script src="npm:@one-data/observable-themes/header.js" defer></script>
-      <script src="npm:@one-data/observable-themes/footer.js" defer></script>`,
+  head: `<link rel="icon" href=${icon} type="image/png" sizes="32x32">`,
 
   root: "src",
-  theme: ["light", "wide", "alt"],
-  toc: false,
-  sidebar: false,
-  pager: false,
+
   style: "style.css",
 
   header: generateHeader({title: "ODA Dashboard"}),
-   footer: "", // what to show in the footer (HTML)
-  // sidebar: true, // whether to show the sidebar
-  // pager: true, // whether to show previous & next links in the footer
-  // output: "dist", // path to the output root for build
-  // search: true, // activate search
-  // linkify: true, // convert URLs in Markdown to links
-  // typographer: false, // smart quotes and other typographic improvements
-  // preserveExtension: false, // drop .html from URLs
-  // preserveIndex: false, // drop /index from URLs
+  footer: generateFooter(),
+
+  toc: false,
+  sidebar: false,
+  pager: false,
 };
