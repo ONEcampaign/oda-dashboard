713348883a5a4ae2beb422f5562240ee2698e501
mharoruiz <88669820+mharoruiz@users.noreply.github.com>
2026-02-20 11:34:49 +0100

    Update dependencies

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,21 +1,18 @@
-import {generateHeader} from "@one-data/observable-themes/header";
-import {generateFooter} from "@one-data/observable-themes/footer";
 import {icon} from "@one-data/observable-themes/use-images";
 
 export default {
   title: "ODA Dashboard",
-  head: `<link rel="icon" href=${icon} type="image/png" sizes="32x32">`,
+  head: `<link rel="icon" href=${icon} type="image/png" sizes="32x32"><script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>`,
+
   base: "/oda-dashboard",
   preserveExtension: true,
 
   root: "src",
-
   style: "style.css",
-
-  header: generateHeader({title: "ODA Dashboard"}),
-  footer: generateFooter(),
 
   toc: false,
   pager: false,
-  sidebar: false
+  sidebar: false,
+  header: false,
+  footer: false,
 };
