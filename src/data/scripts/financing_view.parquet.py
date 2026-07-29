d6443e86b74d770a50468963a8be53bc53e0ab9e
mharoruiz <88669820+mharoruiz@users.noreply.github.com>
2026-07-29 13:55:02 +0200

    Raise error for duplicates

--- src/data/scripts/financing_view.parquet.py
+++ src/data/scripts/financing_view.parquet.py
@@ -97,7 +97,7 @@ def get_dac1():
     ).get_indicators(list(OTHER_FINANCING_INDICATORS))
 
     dac1_raw = pd.concat([in_donor_raw, other_flow_raw, other_ge_raw], ignore_index=True)
-    dac1_raw = resolve_indicator_duplicates(dac1_raw, raise_error=False)
+    dac1_raw = resolve_indicator_duplicates(dac1_raw)
 
     dac1 = (
         dac1_raw.groupby(
